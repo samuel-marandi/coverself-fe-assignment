0162ea5544a9538ce3ccedd9fd8be452bbd8c2d6
Samuel Marandi <samuelcmarandi@gmail.com>
2023-04-05 06:26:25 +0530

    chore: fix css imports

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
 import store from './store/store';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
